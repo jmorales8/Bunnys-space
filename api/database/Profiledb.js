49a7932ab2ae7032ad889f1fb54963c521353ebd
pinhead69 <jaimmoral98@gmail.com>
2024-05-13 01:32:20 -0500

    work on basic db for profile

--- api/database/Profiledb.js
+++ api/database/Profiledb.js
@@ -1,26 +1,28 @@
-const sqlite3 = require('sqlite3').verbose();
-
-let db = new sqlite3.Database('./Profile.db', (err) => {
+import sqlite3 from "sqlite3";
+export let db = new sqlite3.Database("./Profile.db", (err) => {
   if (err) {
-    console.log(err.message);
+    console.log("Database Connection error: ", err.message);
     throw err;
   } else {
-    console.log('Connected to the SQLite db.');
-    db.run(`CREATE TABLE IF NOT EXISTS Profile (
-      id INTEGER PRIMARY KEY AUTOINCREMENT
-      bio text,
-      age integer
-    )`, (err) => {
-      if (!err) {
-        console.log('Table is ready. Inserting initial data.');
+    console.log("Connected to the SQLite db.");
+    db.run(
+      `CREATE TABLE IF NOT EXISTS Profile (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      bio TEXT,
+      age INTEGER
+    )`,
+      (err) => {
+        if (!err) {
+          console.log("Table is ready. Inserting initial data.");
 
-        let insert = 'INSERT INTO Profile (bio, age) VALUES (?,?)'
-        db.run(insert, ["These are my fat nuts", 30]);
-        db.run(insert, ["I cant wait to eat ass", 22]);
+          let insert = "INSERT INTO Profile (bio, age) VALUES (?,?)";
+          db.run(insert, ["These are my fat nuts", 30]);
+          db.run(insert, ["I cant wait to eat ass", 22]);
+        } else {
+          console.error("Error creating table: ", err.message);
+          return;
+        }
       }
-    }
-    )
+    );
   }
-})
-
-module.exports = db;
+});
