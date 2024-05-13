312146bf374680d8dabb4f844c18bb7082be8178
pinhead69 <jaimmoral98@gmail.com>
2024-05-13 01:58:16 -0500

    wip on starting disc server layout

---
+++ ui/src/pages/Discord/Discord.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Bunzone } from "./Servers/Bunzone";
+import { NightRunners } from "./Servers/NightRunners";
+
+export function Discord() {
+  return(
+    <div>
+      <Bunzone />
+      <NightRunners />
+    </div>
+  )
+}
