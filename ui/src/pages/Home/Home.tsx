815013b9beece797edde1d846610c726165f6bb1
pinhead69 <jaimmoral98@gmail.com>
2024-05-17 00:07:03 -0500

    add carousel to home page

--- ui/src/pages/Home/Home.tsx
+++ ui/src/pages/Home/Home.tsx
@@ -1,9 +1,15 @@
+import Carousel from "../../components/ImageCarousel";
 import { Profile } from "./Profile/Profile";
 
+const bruh:string[] = ["bunnyL.jpg", "bunnyL.jpg", "bunnyL.jpg"];
 export function Home() {
   return (
     <div className="home">
+
       <Profile />
+
+      <Carousel images={bruh} />
+
     </div>
   )
 }
