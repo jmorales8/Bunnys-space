e0433d744e809d5ef1d4718611638fdcc92470bf
pinhead69 <jaimmoral98@gmail.com>
2024-05-19 23:28:09 -0500

    wip

--- ui/src/pages/Twitch/TwitchProfile/TwitchProfile.tsx
+++ ui/src/pages/Twitch/TwitchProfile/TwitchProfile.tsx
@@ -3,15 +3,14 @@ import { useEffect, useState } from "react";
 export function TwitchProfile() {
   const [data, setData] = useState({ isLive: false });
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/api/twitch');
+        const response = await fetch("/api/twitch");
         const result = await response.json();
         setData(result);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       } finally {
         setLoading(false);
       }
@@ -22,18 +21,24 @@ export function TwitchProfile() {
   let isUserLive = data.isLive;
   return (
     <div className="profile">
-      {loading ?
+      {loading ? (
         <> loading </>
-        :
+      ) : (
         <>
-          {isUserLive
-            ? <>
-                bruh
-                <img src="bunnyL.jpg" className="twitch__profile__icon__true" />
-              </>
-            : <img src="bunnyL.jpg" className="twitch__profile__icon__false" />}
+          {isUserLive ? (
+            <div className="">
+              <img src="bunnyL.jpg" alt="live" className="twitch__profile__icon__true" />
+            </div>
+          ) : (
+            <div className="twitch__profile__offline">
+              <div className="">
+                <img src="bunnyL.jpg" alt="not live" className="twitch__profile__icon__false" />
+                <div className="twitch__profile__icon__text__offline">Pillow aint streaming :c</div>
+              </div>
+            </div>
+          )}
         </>
-      }
+      )}
     </div>
   );
 }
