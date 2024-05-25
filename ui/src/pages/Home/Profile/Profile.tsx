6c341e66da8b7bffc09d84420ce359bafbde6986
pinhead69 <jaimmoral98@gmail.com>
2024-05-25 13:20:48 -0500

    randomize profile images

--- ui/src/pages/Home/Profile/Profile.tsx
+++ ui/src/pages/Home/Profile/Profile.tsx
@@ -1,11 +1,15 @@
-import React from "react";
 const text = "Anya Forger: The adopted daughter of Loid Forger and Yor Forger, who doesn't know she isn't Loid's biological daughter. She was used as an experiment by the Ostanian government that gave her the ability to read minds. She ended up"
+const profileImages: string[] = ["bunnyW.jpg", "bunnyW2.jpg", "bunnyL.jpg", "bunnyL2.jpg"]
+
+function RandomImage() {
+  return Math.floor(Math.random() * 4)
+}
 
 export function Profile() {
   return (
     <div className="profile">
       <div>
-        <img className="profile__image" src="/images/bunnyW.jpg" alt="bunnie"/>
+        <img className="profile__image" src={profileImages[RandomImage()]} alt="bunnie"/>
       </div>
       <div className="profile__text">
         {text}{text}
