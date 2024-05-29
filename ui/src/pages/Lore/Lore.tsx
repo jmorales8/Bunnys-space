6c11df1b2e4e098d134a3f81c3f2fa4c22653c76
GrpFruit <angel20ramirez@gmail.com>
2024-05-28 19:59:18 -0500

    Add classNames to attributes

--- ui/src/pages/Lore/Lore.tsx
+++ ui/src/pages/Lore/Lore.tsx
@@ -2,12 +2,12 @@ export function Lore() {
   return (
     <div className="lore">
       <div className="lore__content">
-        <h3>LOCATION:</h3>
-        <p>Your moms house</p>
-        <h3>Character:</h3>
-        <p>BUNBUN</p>
-        <h3>Background:</h3>
-        <p>
+        <h3 className="header3">LOCATION:</h3>
+        <p className="paragraph">Your moms house</p>
+        <h3 className="header3">Character:</h3>
+        <p className="paragraph">BUNBUN</p>
+        <h3 className="header3">Background:</h3>
+        <p className="paragraph">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa id neque aliquam vestibulum morbi blandit cursus risus at. Id porta nibh venenatis cras. Lorem mollis aliquam ut porttitor leo a. Id faucibus nisl tincidunt eget. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Porttitor massa id neque aliquam vestibulum morbi blandit. Faucibus interdum posuere lorem ipsum dolor sit amet. Elementum sagittis vitae et leo duis ut diam quam. Leo in vitae turpis massa. Nulla facilisi cras fermentum odio eu feugiat. Auctor elit sed vulputate mi sit amet mauris commodo quis. Ornare suspendisse sed nisi lacus. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Viverra accumsan in nisl nisi. Felis eget nunc lobortis mattis aliquam faucibus purus in massa. Pellentesque diam volutpat commodo sed egestas egestas. Pharetra pharetra massa massa ultricies mi. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium.<br />
 
           Quis auctor elit sed vulputate mi sit. Vestibulum morbi blandit cursus risus at ultrices mi tempus. Elementum facilisis leo vel fringilla est. Odio facilisis mauris sit amet massa vitae tortor condimentum. Leo duis ut diam quam nulla porttitor massa id. Ultricies tristique nulla aliquet enim tortor at auctor urna nunc. Vitae elementum curabitur vitae nunc sed. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Sit amet purus gravida quis blandit turpis. Elit ut aliquam purus sit. Sagittis orci a scelerisque purus semper. Tortor at risus viverra adipiscing. Mattis nunc sed blandit libero volutpat sed. Morbi non arcu risus quis. Aliquet nec ullamcorper sit amet risus. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Risus nec feugiat in fermentum. Vitae justo eget magna fermentum iaculis eu non diam. Blandit libero volutpat sed cras ornare. A diam maecenas sed enim ut sem viverra.<br />
