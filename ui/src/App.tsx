a25969cb83e29d3c683a619b98d8a6c4dc4572ad
pinhead69 <jaimmoral98@gmail.com>
2024-06-02 13:50:23 -0500

    Keep state somewhat

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,20 +1,21 @@
+import React, { useContext, useRef, useState } from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
-import "../src/styles/styles.scss"
+import "../src/styles/styles.scss";
 import { Home } from "./pages/Home/Home";
 import { NavigationBar } from './NavigationBar/NavigationBar';
 import { Twitch } from "./pages/Twitch/Twitch";
 import { Commissions } from "./pages/Commissions/Commissions";
 import { Lore } from "./pages/Lore/Lore";
 import { Discord } from "./pages/Discord/Discord";
-import { Footer } from "./Footer/Footer";
-import Player from "./components/Sound/Player";
-
+import AudioPlayer, { usePersistState } from './components/Sound/Player';
+import { Footer } from './Footer/Footer';
 function App() {
   return (
     <div className="app">
       <NavigationBar />
       <div className="app__content">
         <Router basename={process.env.PUBLIC_URL}>
+        <AudioPlayer/> {/* Ensure Player key remains the same */}
           <Routes>
             <Route path="/" element={<Navigate replace to="/home" />} />
             <Route path="/home" element={<Home />} />
@@ -23,14 +24,12 @@ function App() {
             <Route path="/commissions" element={<Commissions />}/>
             <Route path="/discord" element={<Discord />}/>
           </Routes>
-          <Player />
+
         </Router>
       </div>
       <Footer />
     </div>
   );
 }
-/*
-cd ui && yarn start
-*/
+
 export default App;
---
+++ ui/src/components/Sound/Songs.ts
@@ -0,0 +1,23 @@
+interface SongProps {
+  fileName: string;
+  title: string;
+}
+
+export let Songs: SongProps[] = [
+  {
+    fileName: "Chao_Race_Extended_Mix.mp3",
+    title: "Chao race :3"
+  },
+  {
+    fileName: "Flower_Garden.mp3",
+    title: "Yoshi's Garden"
+  },
+  {
+    fileName: "Live_And_Learn.mp3",
+    title: "Live and LEARN"
+  },
+  {
+    fileName: "Map_BGM.mp3",
+    title: "Yoshi's Map BGM"
+  },
+]
