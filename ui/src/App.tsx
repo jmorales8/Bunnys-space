ce1afde7aac3837e223639c85da8363429927d4e
pinhead69 <jaimmoral98@gmail.com>
2024-07-16 17:49:34 -0500

    bruh

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,13 @@
-import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
-import "../src/styles/styles.scss"
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
+import "../src/styles/styles.scss";
 import { Home } from "./pages/Home/Home";
-import { NavigationBar } from './NavigationBar/NavigationBar';
+import { NavigationBar } from "./NavigationBar/NavigationBar";
 import { Twitch } from "./pages/Twitch/Twitch";
 import { Commissions } from "./pages/Commissions/Commissions";
 import { Lore } from "./pages/Lore/Lore";
@@ -11,16 +17,22 @@ import { Footer } from "./Footer/Footer";
 function App() {
   return (
     <div className="app">
-      <NavigationBar />
       <div className="app__content">
         <Router basename={process.env.PUBLIC_URL}>
+          <div className="app__login">
+            <Link to="/login">
+              <img className="app__login__button" src="/images/hallo_pillow.png" alt="Pillow_corner" />
+            </Link>
+          </div>
+          <NavigationBar />
           <Routes>
+            <Route path="/login" element={<>I am the login page</>} />
             <Route path="/" element={<Navigate replace to="/home" />} />
             <Route path="/home" element={<Home />} />
             <Route path="/lore" element={<Lore />} />
-            <Route path="/twitch" element={<Twitch />}/>
-            <Route path="/commissions" element={<Commissions />}/>
-            <Route path="/discord" element={<Discord />}/>
+            <Route path="/twitch" element={<Twitch />} />
+            <Route path="/commissions" element={<Commissions />} />
+            <Route path="/discord" element={<Discord />} />
           </Routes>
         </Router>
       </div>
