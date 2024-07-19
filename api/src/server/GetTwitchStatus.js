c252ca813032f59b57b558b6e8d8f3fb68261e61
Jaime Morales <jmorales@vailsys.com>
2024-07-19 14:07:17 -0500

    reset backend to api

---
+++ api/src/server/GetTwitchStatus.js
@@ -0,0 +1,13 @@
+// const channelName = 'caseoh_';
+// const channelName = 'kaicenat';
+const channelName = 'locklear';
+export async function GetTwitchStatus() {
+  let a = await fetch(`https://www.twitch.tv/${channelName}`);
+  let isLive = false;
+  if ((await a.text()).includes('isLiveBroadcast')) {
+    isLive = true;
+  } else {
+    console.log("not live");
+  }
+  return isLive;
+}
