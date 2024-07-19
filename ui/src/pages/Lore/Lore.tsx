5f761e47dc9a95612250254c1e4be837e93535a8
pinhead69 <jaimmoral98@gmail.com>
2024-07-19 02:10:54 -0500

    good progress:

--- ui/src/pages/Lore/Lore.tsx
+++ ui/src/pages/Lore/Lore.tsx
@@ -1,36 +1,50 @@
+import { useEffect, useState } from "react";
+
+type LoreTypes = {
+  id: number;
+  text: string;
+  section: number;
+  location: string;
+  character: string;
+  background: string;
+};
+
+type ApiResponse = {
+  lore: LoreTypes[];
+};
+
 export function Lore() {
+  const [lore, setLore] = useState<LoreTypes[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/lore");
+        const result: ApiResponse = await response.json();
+        setLore(result.lore);
+        console.log(result.lore);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="lore">
-      <div className="lore__container">
-      <div id="fade"></div>
-        <div className="lore__container__effect">
-          <div className="lore__container__effect__text">
-            <h3 className="header3">LOCATION:</h3>
-            <p className="paragraph">Your moms house</p>
-            <h3 className="header3">Character:</h3>
-            <p className="paragraph">BUNBUN</p>
-            <h3 className="header3">Background:</h3>
-            <div className="">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo nobis
-              placeat minus, reprehenderit nam perferendis sapiente voluptas fugit
-              cumque ea maxime architecto neque, enim laborum. Corporis asperiores
-              cupiditate quas odio. Lorem ipsum dolor, sit amet consectetur
-              adipisicing elit. Quo nobis placeat minus, reprehenderit nam
-              perferendis sapiente voluptas fugit cumque ea maxime architecto neque,
-              enim laborum. Corporis asperiores cupiditate quas odio. Lorem ipsum
-              dolor, sit amet consectetur adipisicing elit. Quo nobis placeat minus,
-              reprehenderit nam perferendis sapiente voluptas fugit cumque ea maxime
-              architecto neque, enim laborum. Corporis asperiores cupiditate quas
-              odio. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-              nobis placeat minus, reprehenderit nam perferendis sapiente voluptas
-              fugit cumque ea maxime architecto neque, enim laborum. Corporis
-              asperiores cupiditate quas odio. Lorem ipsum dolor, sit amet
-              consectetur adipisicing elit. Quo nobis placeat minus, reprehenderit
-              nam perferendis sapiente voluptas fugit cumque ea maxime architecto
-              neque, enim laborum.
-            </div>
-          </div>
-        </div>
+      <div className="lore__content">
+        <h3 className="header3">LOCATION:</h3>
+        <p className="paragraph"></p>
+        <h3 className="header3">Character:</h3>
+        <p className="paragraph">BUNBUN</p>
+        <h3 className="header3">Background:</h3>
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa id neque aliquam vestibulum morbi blandit cursus risus at. Id porta nibh venenatis cras. Lorem mollis aliquam ut porttitor leo a. Id faucibus nisl tincidunt eget. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Porttitor massa id neque aliquam vestibulum morbi blandit. Faucibus interdum posuere lorem ipsum dolor sit amet. Elementum sagittis vitae et leo duis ut diam quam. Leo in vitae turpis massa. Nulla facilisi cras fermentum odio eu feugiat. Auctor elit sed vulputate mi sit amet mauris commodo quis. Ornare suspendisse sed nisi lacus. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Viverra accumsan in nisl nisi. Felis eget nunc lobortis mattis aliquam faucibus purus in massa. Pellentesque diam volutpat commodo sed egestas egestas. Pharetra pharetra massa massa ultricies mi. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium.<br />
+
+          Quis auctor elit sed vulputate mi sit. Vestibulum morbi blandit cursus risus at ultrices mi tempus. Elementum facilisis leo vel fringilla est. Odio facilisis mauris sit amet massa vitae tortor condimentum. Leo duis ut diam quam nulla porttitor massa id. Ultricies tristique nulla aliquet enim tortor at auctor urna nunc. Vitae elementum curabitur vitae nunc sed. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Sit amet purus gravida quis blandit turpis. Elit ut aliquam purus sit. Sagittis orci a scelerisque purus semper. Tortor at risus viverra adipiscing. Mattis nunc sed blandit libero volutpat sed. Morbi non arcu risus quis. Aliquet nec ullamcorper sit amet risus. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Risus nec feugiat in fermentum. Vitae justo eget magna fermentum iaculis eu non diam. Blandit libero volutpat sed cras ornare. A diam maecenas sed enim ut sem viverra.<br />
+
+          Enim ut sem viverra aliquet eget. Ullamcorper a lacus vestibulum sed arcu non odio euismod. Non consectetur a erat nam at lectus urna duis. Mattis rhoncus urna neque viverra. Lobortis mattis aliquam faucibus purus in massa tempor nec. Volutpat maecenas volutpat blandit aliquam etiam erat velit. Lectus nulla at volutpat diam. Porta nibh venenatis cras sed felis eget. In hac habitasse platea dictumst. Augue ut lectus arcu bibendum at varius vel pharetra vel. Facilisis sed odio morbi quis commodo odio. Eget mi proin sed libero enim sed faucibus. Aliquam faucibus purus in massa tempor nec feugiat. Id aliquet risus feugiat in. Volutpat sed cras ornare arcu dui vivamus arcu felis. Pharetra sit amet aliquam id. Congue nisi vitae suscipit tellus. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Tristique senectus et netus et.
+        </p>
       </div>
     </div>
   )
