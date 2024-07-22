6dc615b6f6ec27f80141017042c412bcf5737587
GrpFruit <angel20ramirez@gmail.com>
2024-07-22 13:03:34 -0500

    Fix git comments

--- ui/src/pages/Lore/Lore.tsx
+++ ui/src/pages/Lore/Lore.tsx
@@ -1,40 +1,37 @@
 export function Lore() {
   return (
-    <>
-      {/* <div id="fade"></div> */}
-      <div className="lore">
-        <div className="lore__container">
-        <div id="fade"></div>
-          <div className="lore__container__effect">
-            <div className="lore__container__effect__text">
-              <h3 className="header3">LOCATION:</h3>
-              <p className="paragraph">Your moms house</p>
-              <h3 className="header3">Character:</h3>
-              <p className="paragraph">BUNBUN</p>
-              <h3 className="header3">Background:</h3>
-              <div className="">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo nobis
-                placeat minus, reprehenderit nam perferendis sapiente voluptas fugit
-                cumque ea maxime architecto neque, enim laborum. Corporis asperiores
-                cupiditate quas odio. Lorem ipsum dolor, sit amet consectetur
-                adipisicing elit. Quo nobis placeat minus, reprehenderit nam
-                perferendis sapiente voluptas fugit cumque ea maxime architecto neque,
-                enim laborum. Corporis asperiores cupiditate quas odio. Lorem ipsum
-                dolor, sit amet consectetur adipisicing elit. Quo nobis placeat minus,
-                reprehenderit nam perferendis sapiente voluptas fugit cumque ea maxime
-                architecto neque, enim laborum. Corporis asperiores cupiditate quas
-                odio. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-                nobis placeat minus, reprehenderit nam perferendis sapiente voluptas
-                fugit cumque ea maxime architecto neque, enim laborum. Corporis
-                asperiores cupiditate quas odio. Lorem ipsum dolor, sit amet
-                consectetur adipisicing elit. Quo nobis placeat minus, reprehenderit
-                nam perferendis sapiente voluptas fugit cumque ea maxime architecto
-                neque, enim laborum.
-              </div>
+    <div className="lore">
+      <div className="lore__container">
+      <div id="fade"></div>
+        <div className="lore__container__effect">
+          <div className="lore__container__effect__text">
+            <h3 className="header3">LOCATION:</h3>
+            <p className="paragraph">Your moms house</p>
+            <h3 className="header3">Character:</h3>
+            <p className="paragraph">BUNBUN</p>
+            <h3 className="header3">Background:</h3>
+            <div className="">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo nobis
+              placeat minus, reprehenderit nam perferendis sapiente voluptas fugit
+              cumque ea maxime architecto neque, enim laborum. Corporis asperiores
+              cupiditate quas odio. Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Quo nobis placeat minus, reprehenderit nam
+              perferendis sapiente voluptas fugit cumque ea maxime architecto neque,
+              enim laborum. Corporis asperiores cupiditate quas odio. Lorem ipsum
+              dolor, sit amet consectetur adipisicing elit. Quo nobis placeat minus,
+              reprehenderit nam perferendis sapiente voluptas fugit cumque ea maxime
+              architecto neque, enim laborum. Corporis asperiores cupiditate quas
+              odio. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
+              nobis placeat minus, reprehenderit nam perferendis sapiente voluptas
+              fugit cumque ea maxime architecto neque, enim laborum. Corporis
+              asperiores cupiditate quas odio. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Quo nobis placeat minus, reprehenderit
+              nam perferendis sapiente voluptas fugit cumque ea maxime architecto
+              neque, enim laborum.
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
