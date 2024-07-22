26a4e3df20b77235e0a285ea6493cfa4a4bff01f
pinhead69 <jaimmoral98@gmail.com>
2024-07-22 02:28:41 -0500

    bruh

--- ui/src/components/ImgsOnNavBar/ImgsOnNavBar.tsx
+++ ui/src/components/ImgsOnNavBar/ImgsOnNavBar.tsx
@@ -1,10 +1,38 @@
 import { useState } from "react";
 
+function RandomImage() {
+  return Math.floor(Math.random() * 100);
+}
+
 export function ImgsOnNavBar() {
-  const [currentImage, setCurrentImage] = useState("")
+  const [isClicked, setIsClicked] = useState(false);
+  const [currImage, setCurrImage] = useState(0);
+  function revertImage() {
+    setIsClicked(false);
+  }
+  const changePic = () => {
+    setCurrImage(RandomImage());
+    setIsClicked(true);
+    setTimeout(revertImage, 300);
+  };
+
   return (
-  <>
-    hi
-  </>
+    <>
+      {!isClicked ? (
+        <div className="navBar__picture" onClick={changePic} />
+      ) : (
+        <>
+          {currImage === 7 ? (
+            <>
+              <div className="navBar__picture__gokufeet" />
+            </>
+          ) : (
+            <>
+              <div className="navBar__picture__necoarc" />
+            </>
+          )}
+        </>
+      )}
+    </>
   );
 }
