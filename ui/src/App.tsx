d00b6cb532e9847b78267680ddfcd5347735592e
pinhead69 <jaimmoral98@gmail.com>
2024-07-29 00:04:27 -0500

    add switch night mode button and dark theme in the back

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -14,30 +14,34 @@ import { Discord } from "./pages/Discord/Discord";
 import { Footer } from "./Footer/Footer";
 import { LoginButton } from "./components/LoginButton/LoginButton";
 import AudioPlayer from "./components/Sound/Player";
+import ThemeProvider from "./context/ThemeContext";
+import { AppContent } from "./components/AppContent/AppContent";
 
 function App() {
   return (
-    <div className="app">
-      <div className="app__content">
-        <Router basename={process.env.PUBLIC_URL}>
-          <div className="app__head">
-            <AudioPlayer />
-            <LoginButton />
-          </div>
-          <NavigationBar />
-          <Routes>
-            <Route path="/login" element={<>I am the login page</>} />
-            <Route path="/" element={<Navigate replace to="/home" />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/lore" element={<Lore />} />
-            <Route path="/twitch" element={<Twitch />} />
-            <Route path="/commissions" element={<Commissions />} />
-            <Route path="/discord" element={<Discord />} />
-          </Routes>
-        </Router>
+    <ThemeProvider>
+      <div className="app">
+        <AppContent>
+          <Router basename={process.env.PUBLIC_URL}>
+            <div className="app__head">
+              <AudioPlayer />
+              <LoginButton />
+            </div>
+            <NavigationBar />
+            <Routes>
+              <Route path="/login" element={<>I am the login page</>} />
+              <Route path="/" element={<Navigate replace to="/home" />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/lore" element={<Lore />} />
+              <Route path="/twitch" element={<Twitch />} />
+              <Route path="/commissions" element={<Commissions />} />
+              <Route path="/discord" element={<Discord />} />
+            </Routes>
+          </Router>
+        </AppContent>
         <Footer />
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
