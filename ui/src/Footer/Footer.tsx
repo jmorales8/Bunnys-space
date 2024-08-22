cab463305ec98088d051ef4da84ba784bc1af39f
GrpFruit <angel20ramirez@gmail.com>
2024-08-21 21:08:06 -0500

    Fix footer css

--- ui/src/Footer/Footer.tsx
+++ ui/src/Footer/Footer.tsx
@@ -7,8 +7,8 @@ interface FooterButtons {
 
 const footerButtons: FooterButtons[] = [
   { label: "Q&A", path: "/Q-and-A" },
-  // { label: "Q&A", path: "/Q-and-A" },
-  // { label: "Q&A", path: "/Q-and-A" },
+  { label: "Q&A", path: "/Q-and-A" },
+  { label: "Q&A", path: "/Q-and-A" },
   { label: "Q&A", path: "/Q-and-A" },
   { label: "Q&A", path: "/Q-and-A" },
   { label: "Q&A", path: "/Q-and-A" },
@@ -19,16 +19,18 @@ export function Footer() {
       <Link className="footer__img" to="/home">
         <img src="/images/peachy.png" width="55" height="55" alt="peach"/>
       </Link>
-      <div className="footer__container">
-        {footerButtons.map((button) => {
-          return (
-            <Link className="footer__button" to={button.path} key={button.label}>
-              {button.label}
-            </Link>
-          )
-        })}
+      <div className="footer__group">
+        <div className="footer__group__buttons">
+          {footerButtons.map((button) => {
+            return (
+              <Link className="footer__button" to={button.path} key={button.label}>
+                {button.label}
+              </Link>
+            )
+          })}
+        </div>
+        <NightModeButton />
       </div>
-      <NightModeButton />
     </footer>
   )
 }
