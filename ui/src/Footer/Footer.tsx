e76edbf8b9a02ccc47c64c6b270a59a349ff755e
GrpFruit <angel20ramirez@gmail.com>
2024-08-25 17:10:11 -0500

    Create post request; Make questionsdb infrastructure

--- ui/src/Footer/Footer.tsx
+++ ui/src/Footer/Footer.tsx
@@ -6,7 +6,7 @@ interface FooterButtons {
   path: string;
 }
 
-const footerButtons: FooterButtons[] = [{ label: "Q&A", path: "/Q-and-A" }];
+const footerButtons: FooterButtons[] = [{ label: "Q&A", path: "/q-and-a" }];
 export function Footer() {
   const themeContext = useContext(ThemeContext);
   if (!themeContext) {
