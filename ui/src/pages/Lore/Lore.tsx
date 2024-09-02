b1a3094fc15c14b0975a19e33fb7a6ebe6c8c8de
GrpFruit <65438123+GrpFruit@users.noreply.github.com>
2024-09-01 19:50:06 -0500

    27-BUNSPACE2024-Add-Button-To-Switch-Fonts (#88)
    * Add button to switch fonts
    
    * Add functionality to button
    
    * WIP
    
    * Button placement and styling
    
    * Add variable changes to textDepth
    
    * WIP

--- ui/src/pages/Lore/Lore.tsx
+++ ui/src/pages/Lore/Lore.tsx
@@ -14,6 +14,7 @@ type ApiResponse = {
 const currentLoreSelection = 0;
 export function Lore() {
   const [lore, setLore] = useState<LoreTypes[]>([]);
+  const [plainText, setPlainText] = useState(false);
   const [loading, setLoading] = useState(true);
   const [sectionedBackground, setSectionedBackground] = useState<string[]>([])
   useEffect(() => {
@@ -31,32 +32,41 @@ export function Lore() {
     };
     fetchData();
   }, []);
-  console.log(lore[currentLoreSelection])
+
+  const isPlainText = () => {
+    setPlainText(!plainText);
+  }
+
+  let textDepth = (depth: string) => plainText ? undefined : 'lore__container__' + [depth];
+
   return (
     <div className="lore">
       {loading ? (
         <> loading </>
       ) : (
-      <div className="lore__container">
-      <div id="fade"></div>
-        <div className="lore__container__effect">
-          <div className="lore__container__effect__text">
-            <h3 className="header3">LOCATION:</h3>
-            <p className="paragraph">{lore[currentLoreSelection].location}</p>
-            <h3 className="header3">Character:</h3>
-            <p className="paragraph">{lore[currentLoreSelection].character}</p>
-            <h3 className="header3">Background:</h3>
-            {sectionedBackground.map((section) => (
-              <div key={section}>
-                <p className="paragraph">
-                  {section}
-                </p>
-                <br />
+        <>
+           <div className="lore__container">
+            {plainText ? null : <div id="fade"/>}
+            <div className={textDepth("effect")}>
+              <div className={textDepth("effect__text")}>
+                <h3 className="header3">LOCATION:</h3>
+                <p className="paragraph">{lore[currentLoreSelection].location}</p>
+                <h3 className="header3">Character:</h3>
+                <p className="paragraph">{lore[currentLoreSelection].character}</p>
+                <h3 className="header3">Background:</h3>
+                {sectionedBackground.map((section) => (
+                  <div key={section}>
+                    <p className="paragraph">
+                      {section}
+                    </p>
+                    <br />
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
-        </div>
-      </div>
+          <button type="button" className="lore__button" onClick={isPlainText}>Switch Text</button>
+        </>
       )}
     </div>
   )
