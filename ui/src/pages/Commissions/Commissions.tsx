217eedc33fe5da34bc22070ab63c83e209c67c69
pinhead69 <jaimmoral98@gmail.com>
2025-03-23 02:17:30 -0500

    decent start

--- ui/src/pages/Commissions/Commissions.tsx
+++ ui/src/pages/Commissions/Commissions.tsx
@@ -1,5 +1,45 @@
+import { useContext } from "react";
+import "./commissions.scss";
+import { ThemeContext } from "../../context/ThemeContext";
+const vtubers = [
+  "Pillow",
+  "AkumaMiko",
+  "Anime4Days",
+  "Cecilia",
+  "BelleCoyote",
+  "Celeste",
+  "Goro",
+  "Liari",
+  "Hanakyo",
+  "Kyoharu",
+  "MissMidgely",
+  "MissMaple",
+  "KillahOrki",
+  "Sprixer",
+  "Misti",
+  "Yozzy",
+  "Natkiki",
+  "RelariaStorm",
+  "Sugie",
+  "YukiShima",
+];
 export function Commissions() {
+  const themeContext = useContext(ThemeContext);
+  if (!themeContext) {
+    throw new Error("DarkMode must be used within a ThemeProvider");
+  }
+  const { isDarkMode } = themeContext;
   return (
-    <h2>THIS IS COMMISSIONS PAGE</h2>
-  )
+    <div className="commissions">
+      {vtubers.map((vtuber) => (
+        <div className="border" key={vtuber}>
+          <img
+            className={isDarkMode ? "commissions__vtubers__night" : "commissions__vtubers"}
+            alt={vtuber}
+            src={`/images/com-vtubers/${vtuber}.png`}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
