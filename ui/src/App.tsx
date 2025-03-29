5437c0e73718c137db3cd4146f24bb27b61c983f
pinhead69 <jaimmoral98@gmail.com>
2025-03-29 02:04:17 -0500

    add query to package.json and optimised discord cuz why not fuck you

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,10 @@
-import React, { useContext, useRef, useState } from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import React, { useContext, useRef, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "../src/styles/styles.scss";
 import { Home } from "./pages/Home/Home";
 import { NavigationBar } from "./NavigationBar/NavigationBar";
@@ -11,32 +16,30 @@ import { Footer } from "./Footer/Footer";
 import { LoginButton } from "./components/LoginButton/LoginButton";
 import ThemeProvider from "./context/ThemeContext";
 import { AppContent } from "./components/AppContent/AppContent";
-import AudioPlayer from './components/Sound/Player';
+import AudioPlayer from "./components/Sound/Player";
 import { Login } from "./pages/Login/Login";
-import BottomDrawer from './components/BottomDrawer/FloatingDrawer';
+import BottomDrawer from "./components/BottomDrawer/FloatingDrawer";
 
 function App() {
   return (
     <ThemeProvider>
-        <AppContent>
-          <Router basename={process.env.PUBLIC_URL}>
-            <div className="app__head">
-              <BottomDrawer />
-              <LoginButton />
-            </div>
-            <NavigationBar />
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/" element={<Navigate replace to="/home" />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/lore" element={<Lore />} />
-              <Route path="/twitch" element={<Twitch />} />
-              <Route path="/commissions" element={<Commissions />} />
-              <Route path="/discord" element={<Discord />} />
-            </Routes>
-            <Footer />
-          </Router>
-        </AppContent>
+      <AppContent>
+        <div className="app__head">
+          <BottomDrawer />
+          <LoginButton />
+        </div>
+        <NavigationBar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/lore" element={<Lore />} />
+          <Route path="/twitch" element={<Twitch />} />
+          <Route path="/commissions" element={<Commissions />} />
+          <Route path="/discord" element={<Discord />} />
+        </Routes>
+        <Footer />
+      </AppContent>
     </ThemeProvider>
   );
 }
--- ui/src/pages/Discord/Discord.tsx
+++ ui/src/pages/Discord/Discord.tsx
@@ -2,46 +2,42 @@ import { useContext, useEffect, useState } from "react";
 import { ServerTemplate } from "./Servers/ServerTemplate/ServerTemplate";
 import { DiscordDataProps } from "./Servers/ServerTemplate/ServerTemplate";
 import { ThemeContext } from "../../context/ThemeContext";
+import { useQuery } from "@tanstack/react-query";
 
 export function Discord() {
-  const [loading, setLoading] = useState(true);
-  const [discordData, setDiscordData] = useState<DiscordDataProps[]>([]);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["discServers"],
+    queryFn: () => fetch("/discord").then((res) => res.json()),
+    staleTime: 1000 * 60 * 5,
+  });
+
   const themeContext = useContext(ThemeContext);
   if (!themeContext) {
     throw new Error("DarkMode must be used within a ThemeProvider");
   }
-  const { isDarkMode } = themeContext;
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/discord");
-        const result = await response.json();
-        console.log(result.Discord.length)
-        setDiscordData(result.Discord);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
 
-    fetchData();
-  }, []);
+  const { isDarkMode } = themeContext;
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {(error as Error).message}</p>;
   return (
     <>
-      {!loading ? (
-        <div className="discord">
-          <div className="discord__servers">
-            {discordData.map((data) => (
-              <div className={isDarkMode ? "discord__server__night" : "discord__server"}>
-                <ServerTemplate key={data.serverID} serverID={data.serverID} title={data.title} description={data.description} joinLink={data.joinLink}/>
-              </div>
-            ))}
-          </div>
+      <div className="discord">
+        <div className="discord__servers">
+          {data.Discord.map((data: DiscordDataProps) => (
+            <div
+              className={isDarkMode ? "discord__server__night" : "discord__server"} key={data.serverID}>
+              <ServerTemplate
+                key={data.serverID}
+                serverID={data.serverID}
+                title={data.title}
+                description={data.description}
+                joinLink={data.joinLink}
+              />
+            </div>
+          ))}
         </div>
-      ) : (
-        <></>
-      )}
+      </div>
     </>
   );
 }
