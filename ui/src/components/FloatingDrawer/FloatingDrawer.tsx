97cd8696b0587b00187180b6434da39ea79a1956
pinhead69 <jaimmoral98@gmail.com>
2025-03-31 15:48:48 -0500

    add new dark mode styling to player

--- ui/src/components/FloatingDrawer/FloatingDrawer.tsx
+++ ui/src/components/FloatingDrawer/FloatingDrawer.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './floatingDrawer.scss';
 import AudioPlayer from '../Sound/Player';
+import { ThemeContext } from '../../context/ThemeContext';
 
 const FloatingDrawer = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDrawer = () => setIsOpen(prev => !prev);
+  const themeContext = useContext(ThemeContext);
+  if (!themeContext) {
+    throw new Error("DarkMode must be used within a ThemeProvider");
+  }
+  const { isDarkMode } = themeContext;
 
   return (
     <>
@@ -16,7 +22,7 @@ const FloatingDrawer = () => {
       </div>
 
       <button
-        className={`floating-toggle ${isOpen ? 'open' : ''}`}
+        className={`floating-toggle ${isOpen ? 'open' : ''} ${isDarkMode ? "floating-toggle__night" : ""}`}
         onClick={toggleDrawer}
       >
         {isOpen ? '⇚' : '⇛'}
