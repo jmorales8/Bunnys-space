3c1f17276d672cc91ac4e1bcd8eb3773c8af88f7
pinhead69 <jaimmoral98@gmail.com>
2026-06-30 01:24:43 -0500

    worked more on carousel

--- da-burrow/app/commissions/page.tsx
+++ da-burrow/app/commissions/page.tsx
@@ -1,35 +1,60 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { EmblaCarousel } from "../components/Carousel/Carousel";
+import Link from "next/link";
 
+const pillowHandle = " @pillow_senpai";
+const pinHandle = " @pinhhead69696969";
 export default function CommissionsPage() {
   return (
     <div className="flex flex-col items-center h-[500px] gap-4 border-4 border-t-0 border-solid border-[#ffc8e9] bg-gradient-to-l from-[#f28989] via-[#f28989] to-[#b8ffc6]">
-      <div className="flex flex-col w-[100%] h-[100%]">
-        <div className="flex justify-between items-center ml-3 mr-3">
-          <div>
+      <div className="flex flex-col w-[100%] h-[100%] justify-evenly">
+        <div className="hidden md:flex justify-around items-center">
+          <div className="min-w-30 max-w-100 w-[20%] flex justify-center">
             <Image
-              src="/commissions/pillbruh.png"
+              src="/commissions/goblin.png"
               alt="pillbleh"
               width={205}
               height={175}
-              className="object-contain ml-2"
+              className="object-contain"
               unoptimized
             />
           </div>
-          <div>
+          <div className="w-[50%] max-w-150 text-black font-bold text-shadow-lg text-wrap">
+            Want a commission? Please DM me either in my Discord DM's or message
+            me in Twitter
+            <Link
+              href="https://x.com/peachi_bunnii"
+              className="text-blue-700 italic"
+            >
+              {pillowHandle}
+            </Link>
+            . If I dont answer, please send a reply to
+            <Link
+              href="https://x.com/pinhead69696969"
+              className="text-blue-700 italic"
+            >
+              {pinHandle}
+            </Link>{" "}
+            on twitter or
+            <span className="text-gray-700"> pinhead69 </span>
+            on discord who is my manager (and the person who designed this
+            website) for a faster reply in case I dont answer right away. <br />{" "}
+            Trust me, he aint doing nothing xd
+          </div>
+          <div className="min-w-30 max-w-100 w-[20%] flex justify-center">
             <Image
               src="/commissions/pillbleh.png"
               alt="pillbleh"
               width={175}
               height={175}
-              className="object-contain ml-2"
+              className="object-contain"
               unoptimized
             />
           </div>
         </div>
-        <div className="flex w-full justify-center">
+        <div className="flex w-full justify-center pr-2 pl-2">
           <EmblaCarousel />
         </div>
       </div>
--- da-burrow/app/components/Carousel/Carousel.tsx
+++ da-burrow/app/components/Carousel/Carousel.tsx
@@ -2,47 +2,104 @@
 
 import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
+import { createPortal } from "react-dom";
+import { useState } from "react";
+import FlowModal from "../modal/modal";
 import "./carousel.css";
 
 const images = [
   "/head.png",
-  "/head.png",
-  "/head.png",
-  "/head.png",
-  "/head.png",
+  "/necoarc_pillow.png",
+  "/pillmurder.png",
+  "/Pillow.png",
+  "/pillpadoru.png",
 ];
 
 export function EmblaCarousel() {
+  const [openedVtuber, setOpenedVtuber] = useState<string>("");
+  const [open, setOpen] = useState<boolean>(false);
   const [emblaRef, emblaApi] = useEmblaCarousel({
     align: "start",
     slidesToScroll: 1,
     loop: true,
   });
 
+  const handleOpenedVtuber = (image: string) => {
+    setOpenedVtuber(image);
+    setOpen(true);
+  };
+
+  const handleClose = () => setOpen(false);
+
   return (
-    <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {images.map((src, index) => (
-            <div className="embla__slide" key={`${src}-${index}`}>
-              <Image
-                src={src}
-                alt={`Carousel image ${index + 1}`}
-                width={200}
-                height={100}
-              />
-            </div>
-          ))}
+    <div className="embla hidden md:flex">
+      <div className="embla__controls">
+        <button
+          type="button"
+          className="embla__button"
+          onClick={() => emblaApi?.scrollPrev()}
+          aria-label="Previous image"
+        >
+          <Image
+            src="/left_skip.png"
+            alt="left"
+            width={75}
+            height={100}
+            className="nonImageHover"
+          />
+        </button>
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {images.map((src, index) => (
+              <div className="embla__slide" key={`${src}-${index}`}>
+                <Image
+                  src={src}
+                  alt={`Carousel image ${index + 1}`}
+                  width={500}
+                  height={250}
+                  className="embla__slideImage w-[100%]"
+                  onClick={() => handleOpenedVtuber(src)}
+                />
+              </div>
+            ))}
+          </div>
         </div>
+        <button
+          type="button"
+          className="embla__button"
+          onClick={() => emblaApi?.scrollNext()}
+          aria-label="Next image"
+        >
+          <Image
+            src="/right_skip.png"
+            alt="left"
+            width={75}
+            height={100}
+            className="nonImageHover"
+          />
+        </button>
       </div>
 
-      <button type="button" onClick={() => emblaApi?.scrollPrev()}>
-        Previous
-      </button>
-
-      <button type="button" onClick={() => emblaApi?.scrollNext()}>
-        Next
-      </button>
+      {open &&
+        createPortal(
+          <div className="commissions__modal" onClick={handleClose}>
+            <div
+              onClick={(event) => event.stopPropagation()}
+            >
+              <FlowModal direction="down" width={"auto"}>
+                <Image
+                  src={openedVtuber}
+                  alt="Opened carousel image"
+                  width={600}
+                  height={600}
+                  className=" nonImageHover rounded-xl flex justify-center align-center items-center hover:cursor-pointer"
+                />
+              </FlowModal>
+            </div>
+          </div>
+          ,
+          document.body,
+        )}
     </div>
   );
 }
